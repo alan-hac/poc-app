0790c241b5bbfa77400eca39e7181da8ffd2046f
Alan Henrique <alan_hac@outlook.com>
2020-02-01 10:05:55 -0300

    Initial Commit

---
+++ src/main/java/br/com/falconer/TwoDatabasesApplication.java
@@ -0,0 +1,13 @@
+package br.com.falconer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoDatabasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoDatabasesApplication.class, args);
+	}
+
+}
